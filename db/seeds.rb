089953faf8a336ba5bf238972c173dddb4b895dc
Javier Soto <sotoseattle@gmail.com>
2014-09-23 13:13:55 -0700

    basic rails blog with fake data

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  Post.create([{
+    title: Faker::Lorem.sentence(3),
+    body: Faker::Lorem.sentence(6, true)}])
+end
