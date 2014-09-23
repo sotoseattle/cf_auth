cdd9aa8c47e2305a089b513ba7e66884bb68590f
Javier Soto <sotoseattle@gmail.com>
2014-09-23 14:32:28 -0700

    add basic auth as in railcast 250

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923200340) do
+ActiveRecord::Schema.define(version: 20140923201622) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
